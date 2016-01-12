ec1547b5805056e07d2dfdeb35229956d8faec62
Wu Huijie <huijie.wu@docomointertouch.com>
2016-01-12 16:32:44 +0800

    Updated styles

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-import React, { Component, View, Text } from 'react-native';
+import React, { Component, View, StyleSheet } from 'react-native';
 
 export default class RNLoaderHUD extends Component {
   render() {
     return (
-      <View>
-        <Text>ok!</Text>
+      <View style={styles.container}>
+        
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(51, 51, 51, 0.5)'
+  }
+});
