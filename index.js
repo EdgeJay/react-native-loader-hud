899a743518589a6b850266e38d995b67b724c04f
Wu Huijie <huijie.wu@docomointertouch.com>
2016-01-12 14:50:21 +0800

    Test class export

---
+++ index.js
@@ -0,0 +1,11 @@
+import React, { View, Text } from 'react-native';
+
+export default class RNLoaderHUD {
+  render() {
+    return (
+      <View>
+        <Text>ok!</Text>
+      </View>
+    );
+  }
+}
